5b9823bea7fc06e8481d05e603ebd65447dadf12
AbdallahAlqadi <161605945+AbdallahAlqadi@users.noreply.github.com>
2025-02-15 21:21:51 +0300

    add

--- Server/Controllers/categoryControllers.js
+++ Server/Controllers/categoryControllers.js
@@ -25,7 +25,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5 ميجابايت
+  limits: { fileSize: 8 * 1024 * 1024 }, // 5 ميجابايت
   fileFilter: (req, file, cb) => {
     const allowedTypes = /jpeg|jpg|png|gif/;
     const ext = allowedTypes.test(
